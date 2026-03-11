34e0f6edd59104f550efc28dad0f301bfb99130b
farzona <aceracerov67@gmail.com>
2026-03-11 14:39:58 +0500

    prefooter1

---
+++ src/pages/Home/Prefooter1.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Prefooter1 = () => {
+  return (
+    <>
+    <div className='prefooter1'>
+        <div className='container'>
+            <div className='prefooter1-container'>
+                <h1>
+                   Dresses you’ll turn to again and again.
+                </h1>
+                <p>
+                    Here is your chance to upgrade your wardrobe with a variation of styles and fits that are both.
+                </p>
+                <button>
+                    View Collection
+                </button>
+                <button>
+                    Add to Bag
+                </button>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Prefooter1
